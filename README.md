df897ab4f83638cd727a683d99089fa05ff769db
redshoga <yui.ichioka.contact@gmail.com>
2019-07-30 23:44:51 +0900

    Add initial files

---
+++ README.md
@@ -0,0 +1,45 @@
+# deepnude4video
+
+DeepNude Source Code for Videos
+
+# How to use
+
+1. Download the model
+
+https://github.com/zhengyima/DeepNude_NoWatermark_withModel
+
+2. Set models in the `/checkpoints` directory
+
+3. Set video to `videos/` directory
+
+4. Build docker machine
+
+```
+docker build -t deepnude/4videos .
+```
+
+5. Exec docker container
+
+```
+docker run --rm -it -v $PWD:/app:rw deepnude/4videos /bin/bash
+docker run --rm -it -v $PWD:/app:rw deepnude/4videos python main.py ./videos/in.mp4 ./videos/out.mp4
+```
+
+# Base repository
+
+https://github.com/zhengyima/DeepNude_NoWatermark_withModel
+
+# GPU Settings
+
+```./lib/gan.py
+- self.gpu_ids = []
++ self.gpu_ids = [0]
+```
+
+# FFMEPG Cheat Sheet
+
+Clipping
+
+```
+ffmpeg -i org.mp4 -ss 249 -t 1 in.mp4
+```
---
+++ main.py
@@ -0,0 +1,65 @@
+import sys
+import cv2
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), './lib'))
+from run import process
+import ffmpeg
+import numpy as np
+
+def deep_nude_process(dress):
+	h = dress.shape[0]
+	w = dress.shape[1]
+	dress = cv2.resize(dress, (512,512), interpolation=cv2.INTER_CUBIC)
+	watermark = process(dress)
+	watermark = cv2.resize(watermark, (w,h), interpolation=cv2.INTER_CUBIC)
+	return watermark
+
+def process_video(input_path, output_path):
+	# https://github.com/kkroening/ffmpeg-python/tree/master/examples
+	probe = ffmpeg.probe(input_path)
+	video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+	width = int(video_stream['width'])
+	height = int(video_stream['height'])
+
+	process1 = (
+		ffmpeg
+		.input(input_path)
+		.output('pipe:', format='rawvideo', pix_fmt='rgb24')
+		.run_async(pipe_stdout=True)
+	)
+
+	process2 = (
+		ffmpeg
+		.input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
+		.output(output_path, pix_fmt='yuv420p')
+		.overwrite_output()
+		.run_async(pipe_stdin=True)
+	)
+
+	while True:
+		in_bytes = process1.stdout.read(width * height * 3)
+		if not in_bytes:
+			break
+		in_frame = (
+			np
+			.frombuffer(in_bytes, np.uint8)
+			.reshape([height, width, 3])
+		)
+
+		out_frame = deep_nude_process(in_frame)
+
+		process2.stdin.write(
+			in_frame
+			.astype(np.uint8)
+			.tobytes()
+		)
+
+	process2.stdin.close()
+	process1.wait()
+	process2.wait()
+
+if __name__ == '__main__':
+	in_path, out_path = sys.argv[1:3]
+	print("Input video:", in_path)
+	print("Output video:", out_path)
+	process_video(in_path, out_path)
